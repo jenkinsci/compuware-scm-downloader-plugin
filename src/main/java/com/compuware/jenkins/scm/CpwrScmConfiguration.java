ec2d3fcc64df8ec90a5f92122596ac696c9e7d3b
Chad Seippel <chadseippel@users.noreply.github.com>
2015-12-29 13:01:06 -0500

    removed old license

--- src/main/java/com/compuware/jenkins/scm/CpwrScmConfiguration.java
+++ src/main/java/com/compuware/jenkins/scm/CpwrScmConfiguration.java
@@ -1,13 +1,3 @@
-/**
- * These materials contain confidential information and trade secrets of Compuware Corporation. You shall maintain the materials
- * as confidential and shall not disclose its contents to any third party except as may be required by law or regulation. Use,
- * disclosure, or reproduction is prohibited without the prior express written permission of Compuware Corporation.
- * 
- * All Compuware products listed within the materials are trademarks of Compuware Corporation. All other company or product
- * names are trademarks of their respective owners.
- * 
- * Copyright (c) 2015 Compuware Corporation. All rights reserved.
- */
 package com.compuware.jenkins.scm;
 
 import hudson.model.BuildListener;
