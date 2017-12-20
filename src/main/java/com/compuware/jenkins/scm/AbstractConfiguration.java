b81ec49c7ea50add5013b11cc854bdf94e411d04
amihzs0 <harry.sellars@compuware.com>
2017-12-20 15:26:58 -0500

    CWE-121581 dependencies

--- src/main/java/com/compuware/jenkins/scm/AbstractConfiguration.java
+++ src/main/java/com/compuware/jenkins/scm/AbstractConfiguration.java
@@ -1,12 +1,18 @@
 /**
- * These materials contain confidential information and trade secrets of Compuware Corporation. You shall maintain the materials
- * as confidential and shall not disclose its contents to any third party except as may be required by law or regulation. Use,
- * disclosure, or reproduction is prohibited without the prior express written permission of Compuware Corporation.
+ * The MIT License (MIT)
  * 
- * All Compuware products listed within the materials are trademarks of Compuware Corporation. All other company or product
- * names are trademarks of their respective owners.
+ * Copyright (c) 2015 - 2018 Compuware Corporation
  * 
- * Copyright (c) 2017 Compuware Corporation. All rights reserved.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice
+ * shall be included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 package com.compuware.jenkins.scm;
 
@@ -96,7 +102,7 @@ public abstract class AbstractConfiguration extends SCM
 			if (connection == null)
 			{
 				String description = m_hostPort + " " + m_codePage; //$NON-NLS-1$
-				connection = new HostConnection(description, m_hostPort, m_codePage, null, null);
+				connection = new HostConnection(description, m_hostPort, m_codePage, null, null, null);
 				globalConfig.addHostConnection(connection);
 			}
 			else
